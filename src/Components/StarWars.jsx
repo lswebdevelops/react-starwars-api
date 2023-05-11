da0b7b70213795f4a3cbed31e739aa519ac321a7
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-10 22:00:10 -0300

    changing character from api call on click

--- src/Components/StarWars.jsx
+++ src/Components/StarWars.jsx
@@ -3,15 +3,26 @@ import React from 'react';
 
 
 function StarWars(){
+    
     const [starWarsData, setStarWarsData] = React.useState({});
+    const [count, setCount] = React.useState(1);
+    
+  
+    // useEffect() >> to avoit an infinity loop
+    React.useEffect(()=>{
+           console.log("effect ran");
+        fetch(`https://swapi.dev/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+        }, [count])
 
-    fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(data => setStarWarsData(data))
 
     return(
         <div className='starwars-container'>
+            <button onClick={()=> setCount(prevCount => prevCount + 1)}>New Character</button>
+            <h2>The count is {count}</h2>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+            
         </div>
     )
 }

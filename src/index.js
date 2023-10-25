077cce8bd40315da2233c6723dc5ce6294c34a35
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-10-25 08:31:07 -0300

    filter using links (buttons)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, useSearchParams } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, useSearchParams, Link } from "react-router-dom";
+import './index.css'
 const swCharacters = [
   { name: "Luke Skywalker", type: "Jedi" },
   { name: "Darth Vader", type: "Sith" },
@@ -12,13 +12,14 @@ function HomePage() {
 
   const [searchParams , setSearchParams ] = useSearchParams()
   const typeFilter = searchParams.get('type');
+  // console.log(typeFilter);
   // console.log(searchParams);  // http://localhost:3000/characters?type=syth >> it console.logs:  //URLSearchParams {size: 1}
   // console.log(searchParams.toString());// http://localhost:3000/characters?type=syth  >> it console.logs: "type=syth"
   //console.log(searchParams.get('type'));// http://localhost:3000/characters?type=syth  >> it console.logs: "syth"
 
   const displayedCharacters = typeFilter
   ? swCharacters.filter(char => char.type.toLowerCase() === typeFilter)
-  :swCharacters
+  : swCharacters
 
   const charEls = displayedCharacters
 
@@ -38,6 +39,13 @@ function HomePage() {
   return (
     <main>
       <h2>Star Wars</h2>
+      <button onClick={() => setSearchParams({type: "jedi"})}>Jedi</button>
+      <button onClick={() => setSearchParams({type: "sith"})}>Sith</button>
+      <button onClick={() => setSearchParams({})}>Clear</button>
+
+      {/* <Link className="filter-van-buttons" to="?type=jedi">Jedi</Link>
+      <Link className="filter-van-buttons" to="?type=sith">Sith</Link>
+      <Link className="filter-van-buttons" to="">All</Link> */}
       {charEls}
     </main>
   );
